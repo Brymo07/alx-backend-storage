b3f9bda7f824ebf81ed7a5fb9d370bbd195be8c9
Felix Darko Owusu <93153635+Brymo07@users.noreply.github.com>
2023-07-14 02:59:56 +0000

    MYSQL advanced

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,17 +1,54 @@
-- creates a stored procedure ComputeAverageWeightedScoreForUsers that computes and store the average weighted score for all students
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
-DELIMITER $$
+-- Create the stored procedure
+DELIMITER //
+
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
-    UPDATE users AS U, 
-        (SELECT U.id, SUM(score * weight) / SUM(weight) AS w_avg 
-        FROM users AS U 
-        JOIN corrections as C ON U.id=C.user_id 
-        JOIN projects AS P ON C.project_id=P.id 
-        GROUP BY U.id)
-    AS WA
-    SET U.average_score = WA.w_avg 
-    WHERE U.id=WA.id;
-END
-$$
+    DECLARE user_id INT;
+    DECLARE total_score FLOAT;
+    DECLARE total_weight FLOAT;
+    DECLARE average_score FLOAT;
+
+    -- Cursor to fetch user IDs
+    DECLARE user_cursor CURSOR FOR SELECT id FROM users;
+
+    -- Declare handlers
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET @done = TRUE;
+
+    OPEN user_cursor;
+
+    -- Loop through each user
+    user_loop: LOOP
+        FETCH user_cursor INTO user_id;
+        IF @done THEN
+            LEAVE user_loop;
+        END IF;
+
+        -- Calculate the total weighted score
+        SELECT SUM(c.score * p.weight) INTO total_score
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        WHERE c.user_id = user_id;
+
+        -- Calculate the total weight
+        SELECT SUM(p.weight) INTO total_weight
+        FROM corrections c
+        JOIN projects p ON c.project_id = p.id
+        WHERE c.user_id = user_id;
+
+        -- Calculate the average weighted score
+        IF total_weight > 0 THEN
+            SET average_score = total_score / total_weight;
+        ELSE
+            SET average_score = 0;
+        END IF;
+
+        -- Update the average weighted score for the user
+        UPDATE users
+        SET average_score = average_score
+        WHERE id = user_id;
+    END LOOP;
+
+    CLOSE user_cursor;
+END //
+
 DELIMITER ;
