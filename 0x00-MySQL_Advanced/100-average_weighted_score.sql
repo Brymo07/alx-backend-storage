f44a44a30ccefece5710e2c39f3b5bb0894493a7
Felix Darko Owusu <93153635+Brymo07@users.noreply.github.com>
2023-07-14 02:52:40 +0000

    MYSQL advanced

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,28 +1,35 @@
 -- Create the stored procedure
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser
-(
-    IN user_id INT
-)
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
 BEGIN
-    -- Declare variables
     DECLARE total_score FLOAT;
     DECLARE total_weight FLOAT;
     DECLARE average_score FLOAT;
 
-    -- Calculate the weighted average score
-    SELECT SUM(score * weight) INTO total_score, SUM(weight) INTO total_weight
-    FROM scores
-    WHERE user_id = user_id;
+    -- Calculate the total weighted score
+    SELECT SUM(c.score * p.weight) INTO total_score
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
 
-    -- Compute the average weighted score
+    -- Calculate the total weight
+    SELECT SUM(p.weight) INTO total_weight
+    FROM corrections c
+    JOIN projects p ON c.project_id = p.id
+    WHERE c.user_id = user_id;
+
+    -- Calculate the average weighted score
     IF total_weight > 0 THEN
         SET average_score = total_score / total_weight;
     ELSE
         SET average_score = 0;
     END IF;
 
-    -- Store the average weighted score for the user
+    -- Update the average weighted score for the user
     UPDATE users
-    SET average_weighted_score = average_score
+    SET average_score = average_score
     WHERE id = user_id;
-END;
+END //
+
+DELIMITER ;
